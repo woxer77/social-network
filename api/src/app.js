eef9b7b3b7e386cdfed58001c39c9dea4219178f
Ohrimenko Denis <woxer77@gmail.com>
2022-08-15 18:12:44 +0300

    feat(api): add cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./services/config');
 const postsRoutes = require('./routes/posts');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/posts', postsRoutes);
